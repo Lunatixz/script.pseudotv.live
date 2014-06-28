e44e600be4330203367b89f8fad6d745c8100c6c
Lunatixz <airies1153@aol.com>
2014-06-28 19:02:16 -0400

    v.0.4.6

--- resources/lib/links.py
+++
@@ -1,4 +0,0 @@
-https://dl.dropboxusercontent.com/s/3e2apr8rpx93ydb/Bumpers.zip
-https://dl.dropboxusercontent.com/s/h6ll48bup03p7kf/PTVL_Color.zip
-https://dl.dropboxusercontent.com/s/ct6dlqxto2vmd5h/PTVL_Mono.zip
-https://dl.dropboxusercontent.com/s/4bdnkjspn6tl2hs/PTVL_Cinema_Experience_Pack.zip
