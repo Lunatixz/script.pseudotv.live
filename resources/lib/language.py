8d888e1f0a78a62717ec074506a4a5c14142c58b
CodeMonkeyZero <CodeMonkeyZero@gmail.com>
2014-07-11 22:08:32 -0700

    GPL Updates
    Updated GLP throughout project

--- resources/lib/language.py
+++ resources/lib/language.py
@@ -1,22 +1,20 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+# Copyright 2011-2014 Jason Anderson, Kevin, Lunatixz, Martijn Kaijser,
+# Tommy Winther, Tristan Fischer.
 #
-#     Copyright (C) 2011-2014 Martijn Kaijser
+# This file is part of PseudoTV Live. <https://github.com/Lunatixz/script.pseudotv.live>.
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 2 of the License, or
-#    (at your option) any later version.
+# PseudoTV Live is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# PseudoTV Live is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# You should have received a copy of the GNU General Public License
+# along with PseudoTV Live. If not, see <http://www.gnu.org/licenses/>.
 
 #import modules
 import xbmc
--- resources/lib/tvdb.py
+++ resources/lib/tvdb.py
@@ -1,22 +1,21 @@
+# Copyright 2011-2014 Jason Anderson, Kevin, Lunatixz, Martijn Kaijser,
+# Tommy Winther, Tristan Fischer.
 #
-#      Copyright (C) 2013 Tommy Winther, Lunatixz, Martijn Kaijser
-#      http://tommy.winther.nu
+# This file is part of PseudoTV Live. <https://github.com/Lunatixz/script.pseudotv.live>.
 #
-#  This Program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2, or (at your option)
-#  any later version.
+# PseudoTV Live is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#  This Program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU General Public License for more details.
+# PseudoTV Live is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with this Program; see the file LICENSE.txt.  If not, write to
-#  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
-#  http://www.gnu.org/copyleft/gpl.html
-#
+# You should have received a copy of the GNU General Public License
+# along with PseudoTV Live. If not, see <http://www.gnu.org/licenses/>.
+
 #import modules
 import sys, re
 import urllib, urllib2
